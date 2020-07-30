54ed0d52099e655b6691d37baacbc8cadeb4efc7
den575 <deniskutchenko008@gamil.com>
2020-07-30 21:20:07 +0200

    first commit

---
+++ MyWebsite/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyWebsite.Models;
+
+namespace MyWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _appEnvironment;
+        public HomeController(IWebHostEnvironment appEnvironment)
+        {
+            _appEnvironment = appEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
